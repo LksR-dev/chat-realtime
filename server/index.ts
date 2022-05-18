a246f0a0a7979de32cbef5a5a7310039d920709f
Lucas Ruiz <90145877+LksR-dev@users.noreply.github.com>
2022-05-18 16:04:43 -0300

    deploy heroku

--- server/index.ts
+++ server/index.ts
@@ -156,6 +156,12 @@ app.post("/rooms/:id", (req, res) => {
   });
 });
 
+app.use(express.static("dist"));
+
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/dist/index.html");
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
