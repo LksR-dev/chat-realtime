7f55e8c532532516b2b059cf98db639cec055209
Lucas Ruiz <90145877+LksR-dev@users.noreply.github.com>
2022-05-19 11:49:01 -0300

    error de paths para el deploy

--- server/index.ts
+++ server/index.ts
@@ -159,13 +159,12 @@ app.post("/rooms/:id", (req, res) => {
 });
 
 const relativeRoute = path.resolve(__dirname, "../dist", "index.html");
-const dist = path.resolve(__dirname, "../dist");
 
 app.get("*", (req, res) => {
   res.sendFile(relativeRoute);
 });
 
-app.use(express.static(dist));
+app.use(express.static("dist"));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
